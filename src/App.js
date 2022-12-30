7ac7faca530709403c209ebd4e97d616520c4329
8Bao6 <bao8696@gmail.com>
2022-12-30 22:45:10 +0700

    ok

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Shoes from "./shoes";
+import ShoesItem from "./shoes/shoesItem";
+
+function App() {
+  return (
+    <div className="App">
+      <Shoes />
+    </div>
+  );
+}
+
+export default App;
